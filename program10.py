a91da82465921c3d2f3f21287dd260ec7eeb8923
ameenps <161990527+ameenps@users.noreply.github.com>
2025-10-08 14:55:45 +0530

    Add files via upload

---
+++ program10.py
@@ -0,0 +1,12 @@
+a = int(input("enter first number : "))
+b = int(input("enter secound number : "))
+c = int(input("enter third number : "))
+
+if a>b and a>c:
+    print("largest number is ", a )
+elif b>a and b>c:
+        print("largest number is :", b)
+
+else:
+      print("largest number is : ", c)
+
