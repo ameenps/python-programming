9ae8f6d97c5de5e24ca4f2029b4ffba61a5f1de7
ameenps <161990527+ameenps@users.noreply.github.com>
2025-10-26 11:36:39 +0530

    Add files via upload

---
+++ program_20.py
@@ -0,0 +1,18 @@
+# finding the factorial of a number 
+def factorial(n):
+    if n == 0 :
+        return 1 
+    else:
+        return n * factorial(n-1)
+    
+num = int(input("enter a number"))
+if num<0:
+    print("no factorials for negative number ")
+
+elif num == 0:
+    print(f"{num}!=1")
+
+else:
+    fact = factorial(num)
+    print(f"{num}={fact}")
+
